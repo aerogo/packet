c0b0004879b0fb2f7ef0652da790dbb0e7f28384
Eduard Urbach <e.urbach@gmail.com>
2019-05-24 15:55:47 +0900

    Fixed spelling mistake

--- IOError.go
+++ IOError.go
@@ -2,7 +2,7 @@ package packet
 
 import "net"
 
-// IOError is the data type for errors occuring in case of failure.
+// IOError is the data type for errors occurring in case of failure.
 type IOError struct {
 	Connection net.Conn
 	Error      error
